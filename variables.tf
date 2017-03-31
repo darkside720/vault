502ab77c3a66397cc78ef9919fbf0cb96b68798f
Anthony J Paragas <ajparagas@gmail.com>
2017-03-31 16:55:39 -0400

    Vault

--- variables.tf
+++ variables.tf
@@ -49,3 +49,10 @@ variable "nodes" {
     default = "2"
     description = "number of Vault instances"
 }
+
+variable "subnets" {
+    description = "list of subnets to launch Vault within"
+}
+
+variable "vpc-id" {
+    description = "VPC ID"
