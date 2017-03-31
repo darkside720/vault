4130059301a48094ca0cd166b2aeaeb44c2a489b
Anthony J Paragas <ajparagas@gmail.com>
2017-03-31 16:53:47 -0400

    Vault

---
+++ subnet.tf
@@ -0,0 +1,8 @@
+# Create the subnet in the vpc
+resource "aws_subnet" "subnet" {
+  vpc_id = "${aws_vpc.vpc.id}"
+  cidr_block = "10.0.0.0/24"
+  tags {
+    "Name" = "vault-test"
+  }
+}
---
+++ vpc.tf
@@ -0,0 +1,9 @@
+variable "aws_vpc" {}
+variable "cidr_block" {}
+
+resource "aws_vpc" "vpc" {
+  cidr_block = "10.0.0.0/16"
+  tags {
+    "Name" = "anthony"
+  }
+}
